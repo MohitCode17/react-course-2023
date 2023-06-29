250792c07a1f30157c67bc5bf2dfe8d84a46b21c
MohitGupta <krishgupta1630@gmail.com>
2023-06-29 22:30:41 +0530

    lect-03

---
+++ 03 markup with jsx/lec03/src/App.jsx
@@ -0,0 +1,18 @@
+import Navbar from "./Navbar";
+import './App.css';
+
+
+export default function App() {
+  return (
+    <Navbar/>
+  )
+};
+
+/* 
+  💯Task
+  Create a navbar in JSX:
+  - Use the semantic `nav` element as the parent wrapper
+  - Have an h1 element with the brand name of your "website"
+  - Insert an unordered list for the other nav elements
+  - Inside the `ul`, have three `li`s for "Pricing", "About", and "Contact"
+*/
---
+++ 03 markup with jsx/lec03/src/Navbar.jsx
@@ -0,0 +1,14 @@
+const Navbar = () => {
+  return (
+    <nav>
+        <h1 className="logo">📚BooksVilla</h1>
+        <ul className="items">
+            <li className="item">Pricing</li>
+            <li className="item">Contact</li>
+            <li className="item">About</li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Navbar;
