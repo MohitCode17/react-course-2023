62cff29f5039546e8cea927327ae8c052f233f4f
MohitGupta <krishgupta1630@gmail.com>
2023-06-29 21:38:40 +0530

    second-lec

---
+++ 02 components/lec02/src/Profile.jsx
@@ -0,0 +1,7 @@
+const Profile = () => {
+    return (
+        <img src="https://simg.nicepng.com/png/small/147-1470274_cat-girl-dress-anime-cat-girl-profile.png" alt="anime-cat-girl" width={"210px"} height={"325px"} />
+    )
+}
+
+export default Profile;
